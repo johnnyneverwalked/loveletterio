d0dcc6d6754cee10cbad4f606a251689c74efb6a
johnnyneverwalked <john.stamtsidis@gmail.com>
2019-01-02 18:32:47 +0200

    credit refs
    chat message send on enter and started game partial
    
    game and deck init and changed rooms to an object
    
    added some game logic and css/html on gameboard (need to fix join)
    
    fixed join issue

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const escape = require('escape-html');
 
 
 app.use(express.static('public'));
@@ -10,19 +11,22 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-let rooms = [];
+let rooms = {};
+
+let deck = [1,1,1,1,1,2,2,3,3,4,4,5,5,6,7,8];
 
 function addPlayer(id, username) {
     return {
         id: id,
         username: username,
         hand: [],
+        discarded: [],
         points: 0,
         isDead: false,
     }
 }
 
-var stringToColor = function(str) {
+let stringToColor = function(str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
@@ -35,12 +39,21 @@ var stringToColor = function(str) {
     return colour;
 };
 
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+//returns an object with the room name roomIndex and the player username
 function findRoomPlayer(socket){
     let data = null;
     Object.keys(socket.rooms).forEach(function (room) {
-        let roomIndex = rooms.findIndex(r => r.id === room);
-        if (roomIndex !== -1) {
-            let player = rooms[roomIndex].players.find(p => p.id === socket.id);
+        // let roomIndex = rooms.findIndex(r => r.id === room);
+        if (rooms[room]) {
+            let player = rooms[room].players.find(p => p.id === socket.id);
 
             if (player){
                 data = {room: room, username: player.username};
@@ -50,24 +63,86 @@ function findRoomPlayer(socket){
     return data;
 }
 
+function getPlayers(room, id) {
+    let players = [];
+    room.players.forEach(function (player) {
+        if(player.id !== id) {
+            players.push({
+                username: player.username,
+                discarded: player.discarded,
+                points: player.points,
+                isDead: player.isDead,
+            })
+        };
+    });
+    console.log(players);
+    return players;
+}
+
+function initGame(room){
+    room.deck = shuffle(deck.slice());
+    room.wildcard = room.deck.shift();
+    room.players.forEach(function (player) {
+        player.hand = [room.deck.shift()];
+        player.discarded = [];
+        player.isDead = false;
+    });
+    room.turn = Math.floor(Math.random()*room.players.length);
+    console.log(room.turn);
+    room.playing = true;
+}
+
+function nextTurn(room){
+    do {
+        room.turn++;
+        if (room.players.length <= room.turn) {
+            room.turn = 0;
+        }
+        if(!room.players[room.turn].isDead){
+            let active = room.players[room.turn];
+            io.to(active.id).emit('yourTurn', {
+                hand: active.hand
+            });
+            room.players.forEach(function (player) {
+                if(player.id !== active.id){
+                    io.to(player.id).emit('nextTurn', {
+                        active_player: active.username
+                    });
+                }
+            });
+            io.in(room.id).emit('updateDeck', {cards_remaining: room.deck.length});
+        }
+    }while (room.players[room.turn].isDead)
+}
+
 io.on('connection', function (socket) {
     console.log('User connected');
 
     //create new game room
     socket.on('createRoom', function (data) {
+        if(!data.username){
+            socket.emit('err', {reason: 'Please enter a username'});
+            return;
+        }
 
         if(data.room_name.length){
-            let room = io.sockets.adapter.rooms[data.room_name];
+            let room_name = escape(data.room_name);
+            let username = escape(data.username);
+            let room = io.sockets.adapter.rooms[room_name];
             if(!room){
-                socket.join(data.room_name);
-                console.log('room: ' +data.room_name+ ', created by ' + data.username);
-                socket.emit('joined', {msg: 'you have joined successfully', player_num: 1});
-
+                socket.join(room_name);
+                console.log('room: ' +room_name+ ', created by ' + username);
+                socket.emit('joined', {
+                    username: username,
+                    player_num: 1,
+                    players: []
+                })
+                socket.emit('logMessage', {msg: 'you have joined successfully'});
                 room = {
-                    id: data.room_name,
-                    players: [addPlayer(socket.id, data.username)]
+                    id: room_name,
+                    players: [addPlayer(socket.id, username)]
                 };
-                rooms.push(room);
+                rooms[room_name] = room;
             }
             else{
                 socket.emit('err', {reason: 'This room already exists'});
@@ -80,24 +155,47 @@ io.on('connection', function (socket) {
 
     //join existing room
     socket.on('joinRoom', function (data) {
-        if(data.username.length === 0){
-            data.username = 'anonymous';
+        if(!data.username){
+            socket.emit('err', {reason: 'Please enter a username'});
+            return;
         }
+
         if(data.room_name.length){
-            let room = io.sockets.adapter.rooms[data.room_name];
+            let room_name = escape(data.room_name);
+            let username = escape(data.username);
+
+            let room = io.sockets.adapter.rooms[room_name];
             if(room) {
-                room = rooms.find(r => r.id === data.room_name);
+                room = rooms[room_name];
             }
             if(room){
-
                 if(room.players.length < 2) { //Should be changed to four once it is working with two
-                    socket.join(data.room_name);
-                    console.log('room: ' + data.room_name + ', joined by ' + data.username);
-                    socket.emit('joined', {msg: 'you have joined successfully', player_num: room.players.length+1});
-                    socket.to(data.room_name).emit('joinedRoom', {name: data.username, player_num: room.players.length+1});
+                    if(room.players.findIndex(p => p.username === username) === -1) {
+                        socket.join(room_name);
+                        room.players.push(addPlayer(socket.id, username));
+                        if(room.playing){
+                            room.players[room.players.length-1].isDead = true;
+                        }
+                        console.log('room: ' + room_name + ', joined by ' + username);
+                        socket.emit('joined', {
+                            player_num: room.players.length + 1,
+                            username: username,
+                            isDead: room.playing,
+                            players: getPlayers(room, socket.id)
+                        });
 
-                    room = rooms.find(r => r.id === data.room_name);
-                    room.players.push(addPlayer(socket.id, data.username));
+                        socket.emit('logMessage', {msg: 'you have joined successfully'});
+                        socket.to(room_name).emit('joinedRoom', {
+                            player_num: room.players.length + 1,
+                            username: username,
+                            isDead: room.playing
+                        });
+
+                        socket.to(room_name).emit('logMessage', {msg: username+' joined the room',});
+                    }
+                    else{
+                        socket.emit('err', {reason: 'Username already taken'});
+                    }
                 }
                 else{
                     socket.emit('err', {reason: 'Room is full'});
@@ -112,18 +210,32 @@ io.on('connection', function (socket) {
         }
     });
 
+    //start game
+    socket.on('startGame', function () {
+        console.log(io.sockets.adapter.rooms);
+        let r = findRoomPlayer(socket);
+        let room = rooms[r.room];
+
+        if(room && room.players.length > 0){// change it back to 1 after testing
+            initGame(room);
+            nextTurn(room);
+        }else{
+            socket.emit('logMessage', {msg: 'Not enough players', color: 'red'})
+        }
+    });
+
     //send a chat message
     socket.on('message', function (data) {
         let r = findRoomPlayer(socket);
         if(r) {
             socket.to(r.room).emit('message', {
                 username: r.username,
-                msg: data.msg,
+                msg: escape(data.msg),
                 color: stringToColor(socket.id)
             });
             socket.emit('message', {
                 username: 'you',
-                msg: data.msg,
+                msg: escape(data.msg),
                 color: stringToColor(socket.id)
             });
         }
@@ -132,17 +244,29 @@ io.on('connection', function (socket) {
     //handle a user leaving
     socket.on('disconnecting',function () {
         Object.keys(socket.rooms).forEach(function (room) {
-            let roomIndex = rooms.findIndex(r => r.id === room);
-            if(roomIndex !== -1) {
-                let playerIndex = rooms[roomIndex].players.findIndex(p => p.id === socket.id);
+            // let roomIndex = rooms.findIndex(r => r.id === room);
+            if(rooms[room]) {
+                let playerIndex = rooms[room].players.findIndex(p => p.id === socket.id);
 
                 if (playerIndex !== -1) {
-                    socket.to(room).emit('userLeft', {msg: rooms[roomIndex].players[playerIndex].username + ' left the room', player_num: rooms[roomIndex].players.length-1});
-                    rooms[roomIndex].players.splice(playerIndex, 1);
+                    socket.to(room).emit('userLeft', {
+                        username: rooms[room].players[playerIndex].username,
+                        player_num: rooms[room].players.length-1
+                    });
+                    socket.to(room).emit('logMessage', {msg: rooms[room].players[playerIndex].username + ' left the room'});
+                    rooms[room].players.splice(playerIndex, 1);
                 }
 
-                if (rooms[roomIndex].players.length === 0)
-                    rooms.splice(roomIndex, 1);
+                if (rooms[room].players.length === 0) {
+                    delete rooms[room];
+                    return;
+                }
+                if(rooms[room].playing && rooms[room].players.length === 1){
+                    socket.to(room).emit('logMessage', {
+                        msg: 'Game ended. Not enough players',
+                        color: 'red'
+                    });
+                }
             }
         });
     });
--- public/js/socket-front.js
+++ public/js/socket-front.js
@@ -2,7 +2,7 @@ $(function () {
 
     var socket = io.connect('http://localhost:1234');
     var players = 0;
-    var username = 'anonymous';
+    var username = null;
 
     var getChat = function(container_id){
         $.ajax({
@@ -16,14 +16,50 @@ $(function () {
                 console.warn(err);
                 location.reload();
             }
-        })
+        });
 
-        $('#send-message').on('click', function () { //send message
-            if($('#new-message').val()){
-                socket.emit('message', {msg: $('#new-message').val()});
+        $('#new-message').on('keydown', function (e) { //don't allow new lines
+            if(e.keyCode === 13){
+                return false;
+            }
+        });
+        
+        $('#new-message').on('keyup', function (e) { //send message with enter
+            let val = $.trim($('#new-message').val());
+            if(e.keyCode === 13 && val){
+                socket.emit('message', {msg: val});
                 $('#new-message').val('');
             }
         });
+
+        $('#send-message').on('click', function () { //send message
+            let val = $.trim($('#new-message').val());
+
+            if(val){
+                socket.emit('message', {msg: val});
+                $('#new-message').val('');
+            }
+        });
+    };
+
+    var getGame = function(container_id){
+        $.ajax({
+            type: 'GET',
+            async: false,
+            url: "partials/_game.html",
+            success: function (html) {
+                $(container_id).append(html);
+            },
+            error: function (err) {
+                console.warn(err);
+                location.reload();
+            }
+        });
+
+        $('#startbtn').on('click', function () {//start game
+            $(this).hide();
+            socket.emit('startGame');
+        })
     };
 
     $('#newbtn').on('click', function () { //create new room
@@ -59,29 +95,81 @@ $(function () {
 
     socket.on('err', function (data) {
         $('#error-container').html('');
-        $('#error-container').append('<p class="error-msg">'+data.reason+'</p>')
+        $('#error-container').append('<p class="error-msg">'+data.reason+'</p>');
     });
 
-    socket.on('joinedRoom', function (data) {
-        players = data.player_num;
-        $('#messages').append('<p name="message">'+data.name + ' joined the room'+'</p>')
+    socket.on('error', function (data) {
+        console.log(data);
     });
 
-    socket.on('joined', function (data) {
-        $('#menu').remove();
-        getChat('#container');
-        players = data.player_num;
-        $('#messages').append('<p name="message">'+data.msg+'</p>')
+    socket.on('logMessage', function (data) {
+        $('#messages').append('<p name="message">'+data.msg+'</p>');
+        if(data.hasOwnProperty('color')){
+            $('#messages p:last-child').css('color', data.color);
+        }
     });
 
     socket.on('message', function (data) {
-        $('#messages').append('<p name="message">'+data.username+': '+data.msg+'</p>')
+        $('#messages').append('<p name="message">'+data.username+': '+data.msg+'</p>');
         $('#messages p:last-child').css({color: data.color, fontWeight: 'bold'});
+    });
+
+    socket.on('joinedRoom', function (data) {
+        $('#opponents .player-container').each(function () {
+            if(!$(this).attr('id')){
+                $(this).attr('id', data.username);
+                $(this).find('.player-card').css('background', data.isDead ? 'gray' : 'mediumpurple').html('')
+                    .append('<h4>player: '+data.username+'</h4>')
+                    .append('<h4 name="points">points: 0</h4>');
+
+                return false;
+            }
+        });
+
+        players = data.player_num;
+    });
+
+    socket.on('joined', function (data) {
+        $('#homemenu').remove();
+        getChat('#container');
+        getGame('#container');
+        $('#you .player-card').css('background', data.isDead ? 'gray' : 'mediumpurple')
+            .append('<h4>player: ' + data.username + '</h4>')
+            .append('<h4 name="points">points: 0</h4>');
+
+        data.players.forEach(function (player) {
+            $('#opponents .player-container').each(function () {
+                if (!$(this).attr('id')) {
+                    $(this).attr('id', player.username);
+                    $(this).find('.player-card').css('background', player.isDead ? 'gray' : 'mediumpurple').html('')
+                        .append('<h4>player: ' + player.username + '</h4>')
+                        .append('<h4 name="points">points: ' + player.points + '</h4>');
+
+                    return false;
+                }
+            });
+
+            players = data.player_num;
+
+        });
+    });
+
+    socket.on('yourTurn', function (data) {
+
+    });
+
+    socket.on('nextTurn', function (data) {
+    });
+
+    socket.on('updateDeck', function (data) {
+        $('#deck').html('<h1>'+data.cards_remaining+'</h1>');
+        if(data.cards_remaining === 0){
+            $('#deck').removeClass('card-back').addClass('empty-deck');
+        }
     });
 
     socket.on('userLeft', function (data) {
         players = data.player_num;
-        $('#messages').append('<p name="message">'+data.msg+'</p>');
     });
 
 });
